6c813df27067b32f013484feac2856e19d7a4f53
TaikiSWS <syssc.taiki@hotmail.com>
2017-04-19 18:17:00 -0300

    Whole ||Basics programs for starting in the Python
    If you have any suggestion send it to my mail: syssc.taiki@hotmail.com
    
    This program is created for Taiki

---
+++ Whole.py
@@ -0,0 +1,20 @@
+number_one = 20
+number_two = 10
+
+result = number_one + number_two
+print("Suma", result);
+
+result = number_one - number_two
+print("Resta", result);
+
+result = number_one * number_two
+print("Multiplicacion", result);
+
+result = number_one ** number_two #Regresa al ejecutar NUMERO EXPONENCIAL
+print("Exponencial", result);
+
+result = number_one / number_two #Regresa al ejecutar NUMERO FLOTANTE
+print("Division", result);
+
+result = number_one // number_two #Regresa al ejecutar NUMERO ENTERO
+print("Division", result);
